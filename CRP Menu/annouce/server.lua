fa922c38aa179d39bd6b4e55eee6959f9dfeefae
ReachardKing <67172940+ReachardKing@users.noreply.github.com>
2021-08-09 04:15:57 -0400

    Update server.lua

--- CRP Menu/annouce/server.lua
+++ CRP Menu/annouce/server.lua
@@ -3,3 +3,6 @@ AddEventHandler('announce', function(param)
   print('^7[^1Announcement^7]^5:'.. param)
   TriggerClientEvent('chatMessage', -1, '^7[^1Announcement^7]^2', {0,0,0} --[[ this table is just rgb ]], param)
 end)
+
+
+print("CRPMenu Made by Mackenzie_Rich & Desired (CRPMenu)")
