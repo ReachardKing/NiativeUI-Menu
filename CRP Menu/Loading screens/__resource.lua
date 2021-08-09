ab2969d794d9b029975b3b72837332a6b43418c2
ReachardKing <67172940+ReachardKing@users.noreply.github.com>
2021-08-09 04:23:45 -0400

    Update __resource.lua

--- CRP Menu/Loading screens/__resource.lua
+++ CRP Menu/Loading screens/__resource.lua
@@ -1,6 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-print("CRPMenu Made by Mackenzie_Rich & Desired (CRPMenu)")
+print("loadingscreens Made by Mackenzie_Rich & Desired (loadingscreens)")
 
 loadscreen "html/index.html"
 
