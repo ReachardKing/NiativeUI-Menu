b6dfe12cd2ab1c85bc054f7fee9b3c1186ef469c
ReachardKing <67172940+ReachardKing@users.noreply.github.com>
2021-10-22 08:57:27 -0400

    Update resouce.lua

--- FireFighteMenu/resouce.lua
+++ FireFighteMenu/resouce.lua
@@ -1,1 +1,6 @@
-client_script "FireFighteMenu"
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+client_script {
+    "@NativeUI/NativeUI.lua",
+    "FireFighter.lua", 
+    -- "config.lua"
+}
