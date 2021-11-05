bce345febc2a3de4433eb10422edb2a7f7edbbf1
ReachardKing <67172940+ReachardKing@users.noreply.github.com>
2021-11-05 15:01:17 -0400

    Update resouce.lua

--- FireFighteMenu/resouce.lua
+++ FireFighteMenu/resouce.lua
@@ -1,8 +1,9 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 version "1.9.1"
 client_script {
-    "@NativeUI/NativeUI.lua",
+    "dependency/NativeUI.lua",
     "FireFighter.lua", 
     "Version-Checker.lua"
     -- "config.lua"
 }
+## put the duble under score before the name resouces
