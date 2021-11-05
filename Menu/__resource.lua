9c71dc61ae11ed1cb9cc2dd3f2d4c181033d593d
ReachardKing <67172940+ReachardKing@users.noreply.github.com>
2021-11-05 14:59:23 -0400

    Update __resource.lua

--- Menu/__resource.lua
+++ Menu/__resource.lua
@@ -1,7 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 version '1.7.1'
 client_scripts {
-    "@NativeUI/NativeUI.lua",
+    "dependency/NativeUI.lua",
     "CRP Menu.lua",
     "Version-Checker.lua"
     -- "config.lua"
