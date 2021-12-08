e6a1e11328682a194e4fdd0b1ec632ad4601f5c5
ReachardKing <67172940+ReachardKing@users.noreply.github.com>
2021-12-08 09:46:19 -0500

    Update __resource.lua

--- Menu/__resource.lua
+++ Menu/__resource.lua
@@ -1,5 +1,5 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-version '1.7.3'
+version '1.7.6'
 client_scripts {
     "dependency/*", --untick this if you do not have NativeUI folder
     -- "@NativeUI/NativeUI.lua",
