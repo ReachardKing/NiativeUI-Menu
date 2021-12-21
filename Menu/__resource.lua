06c010cd606f3a2f1531f2ce004484e0a20df4db
ReachardKing <67172940+ReachardKing@users.noreply.github.com>
2021-12-21 08:11:24 -0500

    Update __resource.lua

--- Menu/__resource.lua
+++ Menu/__resource.lua
@@ -1,7 +1,8 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 version '1.7.6'
+--untick any of thes, make sure you have NativeUI installed
 client_scripts {
-    "dependency/*", --untick this if you do not have NativeUI folder
+    --"dependency/*", 
     -- "@NativeUI/NativeUI.lua",
     "client.lua",
     "CRP Menu.lua",
